6e1025668bbd442d6ce7cc57ddb8e6e3a585f17d
shanu2409 <shanusingh2409@gmail>
2023-06-26 22:41:40 +0530

    Add project files.

---
+++ BlogiVerseAPI/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogiVerseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsData = new string[] { "shanu", "raj", "shane", "niraj" };
+
+            return Ok(studentsData);
+        }
+    }
+}
---
+++ BlogiVerseAPI/Data/BlogiVerseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BlogiVerseAPI.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BlogiVerseAPI.Data
+{
+    public class BlogiVerseDbContext : DbContext
+    {
+        public BlogiVerseDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        /*public DbSet<Image> Images { get; set; }*/
+    }
+}
